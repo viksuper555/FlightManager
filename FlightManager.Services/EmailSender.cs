9f4de081fba65eb1ed6f7275561627010930bacb
Viktor Kolev <viksuper555@gmail.com>
2020-11-02 21:04:56 +0200

    Updated Email Service, using SendGridApi

--- FlightManager.Services/EmailSender.cs
+++ FlightManager.Services/EmailSender.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Net.Mail;
 using System.Net.Mime;
 using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace FlightManager.Services
 {
@@ -22,50 +24,29 @@ namespace FlightManager.Services
         {
             this.Configuration = configuration;
             email = Configuration.GetValue<string>("EmailSender:EmailAddress");
-            username = Configuration.GetValue<string>("EmailSender:Username");
+            username = Configuration.GetValue<string>("EmailSender:Name");
             password = Configuration.GetValue<string>("EmailSender:Password");
 
         }
+        
         public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            var toAddress = new MailAddress(email);
-            var fromAddress = new MailAddress(this.email, username);
+            var toAddress = new EmailAddress(email);
+            var fromAddress = new EmailAddress(this.email, username);
 
-            /* var smtp = new SmtpClient
-             {
-                 Host = "smtp.gmail.com",
-                 Port = 587,
-                 EnableSsl = true,
-                 DeliveryMethod = SmtpDeliveryMethod.Network,
-                 UseDefaultCredentials = false,
-                 Credentials = new NetworkCredential(fromAddress.Address, password),
-                 Timeout = 20000
-             };*/
             try
             {
-                using var emailMessage = new MailMessage(fromAddress, toAddress)
-                {
-                    IsBodyHtml = true,
-                    Subject = subject,
-                    Body = htmlMessage
-                };
+                var apiKey = Configuration.GetValue<string>("EmailSender:APIKey");
+                var client = new SendGridClient(apiKey);
 
-                emailMessage.To.Add(toAddress);
-
-                emailMessage.From = fromAddress;
-
-                SmtpClient smtpClient = new SmtpClient("smtp.sendgrid.net", Convert.ToInt32(587));
-                System.Net.NetworkCredential credentials = new System.Net.NetworkCredential(
-                    Configuration.GetValue<string>("EmailSender:EmailAddress"), 
-                    Configuration.GetValue<string>("EmailConfig:APIKey"));
-                smtpClient.Credentials = credentials;
-
-                await smtpClient.SendMailAsync(emailMessage);
+                var emailMessage = MailHelper.CreateSingleEmail(fromAddress, toAddress, subject, htmlMessage, htmlMessage.ToString());
+                var response = await client.SendEmailAsync(emailMessage);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
+
         }
     }
 }
